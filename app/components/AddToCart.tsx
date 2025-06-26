0a992e8382799ef549e97c619ff17c95adf951fe
Kebhino <wrobeljakub@gmail.com>
2025-06-26 22:21:39 +0200

    Jelsi wyłaczymy cache to komponent staje się dynamiczny i renderowany po stronie clienta

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return <button onClick={() => console.log("Click")}> ProductCard</button>;
+};
+
+export default AddToCart;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1> Hello World</h1>
-      <Link href="/users">LINK</Link>
+      <Link href="/users">User Page</Link>
+      <ProductCard />
     </main>
   );
 }
