2d887c95f538622c571f2ce22d7b6498686d733a
Kebhino <wrobeljakub@gmail.com>
2025-07-07 10:02:14 +0200

    Catch-all-Segments

---
+++ app/products/[[...slug]]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface Props {
+  params: {
+    slug: string[];
+  };
+}
+
+const ProductPage = (props: Props) => {
+  return <div> ProductPage {props.params.slug}</div>;
+};
+
+export default ProductPage;
