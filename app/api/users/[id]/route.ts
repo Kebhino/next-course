6ee2a812093b9f0dd704e47df722483230bad561
Kebhino <wrobeljakub@gmail.com>
2025-07-08 22:47:02 +0200

    lekarz

---
+++ app/api/users/[id]/route.ts
@@ -0,0 +1,12 @@
+// import { NextRequest } from "next/server";
+
+// interface Props {
+//   params: Promise<{
+//     id: number;
+//   }>;
+// }
+
+// export function GET(request: NextRequest, {params}: Props) {
+//    if (id > 10) return Next.NextResponse
+
+// }
