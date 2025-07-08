47ff864026db1cbd40f77bac483655465085b915
Kebhino <wrobeljakub@gmail.com>
2025-07-08 13:59:11 +0200

    API - Get Objects

---
+++ app/api/users/route.ts
@@ -0,0 +1,5 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest) { 
+    return NextResponse.json([{id:1, name: `Mosh`}, {id: 2, name: `John`}]);
+}
